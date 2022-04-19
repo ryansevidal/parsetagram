06dbdc8b311404ef717034e95c865948adb82ca4
ryansevidal <89810484+ryansevidal@users.noreply.github.com>
2022-04-18 21:08:13 -0400

    part 2

--- parsetagram/FeedViewController.swift
+++ parsetagram/FeedViewController.swift
@@ -8,19 +8,96 @@
 import UIKit
 import FirebaseFirestore
 import Firebase
+import SceneKit
+import InputBarAccessoryView
 
-class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, InputBarAccessoryViewDelegate {
+    
+    
 
     @IBOutlet weak var tableView: UITableView!
     
     var posts:[[String:Any]] = [[String:Any]]()
+    var inputBar: InputBarAccessoryView = InputBarAccessoryView()
+    var showsInputBar = false
+    var selectedPost:Int = -1
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        tableView.keyboardDismissMode = .interactive
+        
+        inputBar.delegate = self
+        inputBar.inputTextView.keyboardType = .twitter
+        inputBar.inputPlugins = []
+        
+        inputBar.inputTextView.placeholder = "Add a comment..."
+        inputBar.sendButton.title = "Post"
+        
+        let center = NotificationCenter.default
+        center.addObserver(self, selector: #selector(keyboardWillBeHidden(note:)), name: UIResponder.keyboardWillHideNotification, object: nil)
         // Do any additional setup after loading the view.
+    }
+    
+    @objc func keyboardWillBeHidden(note: Notification) {
+        inputBar.inputTextView.text = nil
+        showsInputBar = false
+        becomeFirstResponder()
+    }
+    
+    override var inputAccessoryView: UIView? {
+        return inputBar
+    }
+    
+    override var canBecomeFirstResponder: Bool {
+        return showsInputBar
+    }
+    
+    func inputBar(_ inputBar: InputBarAccessoryView, didPressSendButtonWith text: String) {
+        if selectedPost >= 0 && selectedPost < self.posts.count {
+            var post = self.posts[selectedPost]
+            
+            var newComment:[String:Any] = [String:Any]()
+            newComment["text"] = text
+            
+            guard let user = Firebase.Auth.auth().currentUser, let username = user.email else {
+                print("cannot set author of post")
+                return
+            }
+            newComment["author"] = String(username[..<(username.firstIndex(of: "@") ?? username.endIndex)])
+            newComment["authorUID"] = user.uid
+            
+            var comments:[[String:Any]] = [[String:Any]]()
+            if post["comments"] != nil{
+                comments = post["comments"] as! [[String:Any]]
+            }
+            
+            comments.append(newComment)
+            let postRef = Firestore.firestore().collection("posts").document(post["docID"] as! String)
+            postRef.updateData(["comments": comments]) {
+                error in
+                if let err = error {
+                    print("error adding comment")
+                }
+                else {
+                    print("comment successfully added")
+                    post["comments"] = comments
+                    self.posts[self.selectedPost] = post
+                    self.tableView.reloadData()
+                }
+                
+                self.selectedPost = -1
+                
+                self.inputBar.inputTextView.text = nil
+                self.showsInputBar = false
+                self.becomeFirstResponder()
+                
+                self.inputBar.inputTextView.resignFirstResponder()
+            }
+        }
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -36,34 +113,90 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
             
             for document in querySnapshot!.documents {
                 self.posts.append(document.data())
+                self.posts[self.posts.count - 1]["docID"] = document.documentID
+                
             }
             
             self.tableView.reloadData()
         }
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {
         return posts.count
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let post = self.posts[section]
+        return (post["comments"] as? [[String:Any]] ?? []).count + 2
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        let post = self.posts[indexPath.section]
         
-        let post = self.posts[indexPath.row]
-        cell.usernameLabel.text = post["author"] as? String ?? ""
-        cell.captionLabel.text = post["caption"] as? String ?? ""
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+            
+            cell.usernameLabel.text = post["author"] as? String ?? ""
+            cell.captionLabel.text = post["caption"] as? String ?? ""
         
-        guard let url = URL(string: "\(post["image"] as? String ?? "")" ) else {
-            print("can't get image url")
-            cell.photoView.image = UIImage(named: "image_placeholder")
+            guard let url = URL(string: "\(post["image"] as? String ?? "")" ) else {
+                print("can't get image url")
+                cell.photoView.image = UIImage(named: "image_placeholder")
+                return cell
+            }
+        
+            cell.photoView.af.setImage(withURL: url)
             return cell
         }
-        
-        cell.photoView.af.setImage(withURL: url)
-        return cell
+        else {
+            if let comments = (post["comments"] as? [[String:Any]]), indexPath.row < comments.count + 1 {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "CommentCell") as! CommentCell
+                let comment = comments[indexPath.row - 1]
+                cell.commentLabel.text = comment["text"] as? String ?? "No Comment Found"
+                cell.nameLabel.text = comment["author"] as? String ?? "None"
+                return cell
+            }
+            else {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "AddCommentCell")
+                return cell!
+            }
+        }
     }
         
+    @IBAction func onLogoutButton(_ sender: Any) {
+        do {
+            try Firebase.Auth.auth().signOut()
+        }
+        catch {
+            print("No user signed in!")
+        }
         
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate = windowScene.delegate as? SceneDelegate else { return }
+        
+        delegate.window?.rootViewController = loginViewController
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        var post = self.posts[indexPath.section]
+        if var comments = post["comments"] as? [[String:Any]] {
+            if indexPath.row == comments.count + 1 {
+                showsInputBar = true
+                becomeFirstResponder()
+                inputBar.inputTextView.becomeFirstResponder()
+                selectedPost = indexPath.section
+            }
+        }
+        else if indexPath.row == 1 {
+            showsInputBar = true
+            becomeFirstResponder()
+            inputBar.inputTextView.becomeFirstResponder()
+            selectedPost = indexPath.section
+        }
+        
+    }
+    
     
     
     /*
--- parsetagram/SceneDelegate.swift
+++ parsetagram/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -13,10 +14,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        if let currentUser = Firebase.Auth.auth().currentUser {
+            print("Signed in as user: \(currentUser.email)")
+            let main = UIStoryboard(name: "Main", bundle: nil)
+            let feedNavigationController = main.instantiateViewController(withIdentifier: "FeedNavigationController")
+            window?.rootViewController = feedNavigationController
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
